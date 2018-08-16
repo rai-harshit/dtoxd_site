93b78b4a6e3a6a740c56b556eccb569af299c910
Harshit Rai <harshitrai68@gmail.com>
2018-08-16 22:28:19 +0530

    Added Creds

--- submit.php
+++ submit.php
@@ -1,9 +1,18 @@
 <?php 
 
-$host = "";
-$port = "";
-$username = "";
-$password = "";
+$host = "ec2-54-235-242-63.compute-1.amazonaws.com";
+$dbname = "da1q0ce15evmg7";
+$port = "5432";
+$username = "drimiljykoeytx";
+$password = "3ed0da850d203469b13f2477372dde01a64d11afd1d1a3fccf00285675023cc9";
+
+$db_connection = pg_connect("host=$host dbname=$dbname port=$port user=$username password=$password");
+
+if(!$db_connection)
+{
+	print("Could Not Connect to the Database. Try again later.");
+}
+
 $name = $_POST['name'];
 $email = $_POST['email'];
 $contact = $_POST['contact'];
